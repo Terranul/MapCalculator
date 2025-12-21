b09ff60449276b47f8feaa9e53798ea63bcaceb2
Ben Faraone <bfaraone@students.cs.ubc.ca>
2025-12-21 11:19:57 -0800

    Initial Commit

---
+++ SwiftUi Mapkit test/SwiftUi_Mapkit_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_Mapkit_testApp.swift
+//  SwiftUi Mapkit test
+//
+//  Created by Ben Faraone on 2025-12-21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_Mapkit_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUi Mapkit testTests/SwiftUi_Mapkit_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_Mapkit_testTests.swift
+//  SwiftUi Mapkit testTests
+//
+//  Created by Ben Faraone on 2025-12-21.
+//
+
+import Testing
+@testable import SwiftUi_Mapkit_test
+
+struct SwiftUi_Mapkit_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
